d0d237b5e5df6b3a4c68c15cfce107797c914a8e
DacHieuDz <116139910+DacHieuDz@users.noreply.github.com>
2022-12-19 23:58:34 +0700

    upload

--- robot/js/script.js
+++ robot/js/script.js
@@ -21,3 +21,12 @@ for(let i=0;i<menuList.length;i++) {
 		}
 	}
 }
+var commentClick = document.querySelector("#comment-click");
+commentClick.onclick = function() {
+	var valueSpan = commentClick.nextElementSibling;
+	if(valueSpan.innerHTML.toString().length == 0) {
+		valueSpan.innerHTML = "Oops! There was a problem.";
+	}else {
+		valueSpan.innerHTML = "";
+	}
+}
